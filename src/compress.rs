c6fc88340bf141072463cb0ed779f7f2b7dc0825
Ganesh Kumar <mganeshkumar269@gmail.com>
2025-06-03 12:17:05 +0530

    recommiting

--- src/compress.rs
+++ src/compress.rs
@@ -1,3 +1,80 @@
-pub fn compress(bytes: &[u8]) -> Result<&[u8], &str>{
-    Err("TODO")
+
+struct FrameHeader{
+    frame_header_descriptor: u8,
+    window_descriptor: u8, // optional
+    dictionary_id: u32, // optional
+    frame_content_size: u64
+}
+
+pub fn get_frame_header_as_bytes(
+    frame_content_size_flag: u8,
+    single_segment_flag: bool,
+    content_checksum_flag: bool,
+    dictionary_id_flag: u8,
+    frame_content_size: u64
+) -> Vec<u8>{
+    let frame_header_descriptor = (
+        (
+            (
+                (
+                    (frame_content_size_flag << 1)
+                    | (single_segment_flag as u8)
+                ) << 3
+            ) | (content_checksum_flag as u8)
+        ) << 2
+    ) | dictionary_id_flag;
+
+    let mut res = Vec::from(frame_header_descriptor.to_le_bytes());
+    res.append(&mut Vec::from(frame_content_size.to_le_bytes()));
+    res
+}
+
+
+#[derive(Default, Debug)]
+pub struct Block {
+    block_header:  u32,
+    block_content: Vec<u8>,
+}
+
+pub enum BlockType {
+    RAW_BLOCK,
+    RLE_BLOCK,
+    COMPRESSED_BLOCK,
+    RESERVED
+}
+
+impl BlockType {
+    fn value(self) -> u8 {
+        match (self) {
+            BlockType::RAW_BLOCK => 0,
+            BlockType::RLE_BLOCK => 1,
+            BlockType::COMPRESSED_BLOCK => 2,
+            BlockType::RESERVED => 3
+        }
+    }
+}
+
+
+impl Block {
+    pub fn set_block_header(&mut self, is_last_block: bool, block_type: BlockType, block_size: u32) {
+        self.block_header = ((block_size << 3) | (block_type.value() as u32)) | (is_last_block as u32);
+    }
+
+    fn block_header_as_bytes(self) -> Vec<u8> {
+        self.block_header.to_le_bytes()[0..3].to_vec()
+    }
+
+
+}
+
+struct Frame {
+    magic_numer: u32, // const to 0xFD2FB528
+    frame_header: FrameHeader,
+    data_block: Block,
+    content_checksum: u32
+}
+
+
+pub fn compress(bytes: Vec<u8>) -> Result<Vec<u8>, String>{
+    Err(String::from("TODO"))
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,41 @@
 mod compress;
 
-pub fn compress(bytes: &[u8]) -> Result<&[u8], &str> {
-    Err("TODO")
+pub fn compress(bytes: Vec<u8>) -> Result<Vec<u8>, String> {
+    Err(String::from("TODO"))
 }
 
-pub fn decompress(bytes: &[u8]) -> Result<&[u8], &str> {
-    Err("TODO")
+pub fn decompress(bytes: Vec<u8>) -> Result<Vec<u8>, String> {
+    Err(String::from("TODO"))
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::compress::{get_frame_header_as_bytes, Block, BlockType};
     use super::*;
 
     #[test]
     fn test_compress() {
-        let compressed_data = compress("example_input".as_bytes());
+        let compressed_data = compress("example_input".as_bytes().to_vec());
     }
 
     #[test]
     fn test_decompress() {
-        let decompressed_data = decompress("example_input".as_bytes());
+        let decompressed_data = decompress("example_input".as_bytes().to_vec());
     }
+
+    #[test]
+    fn sample_test(){
+        // let mut a = Block::default();
+        // a.set_block_header(false, BlockType::RAW_BLOCK, 2);
+        // println!("{:?}", a);
+        // println!("{:?}", &1024_i32.to_be_bytes()[0..3].to_vec());
+        // println!("{:?}", &1024_i32.to_be_bytes().to_vec()[0..3]);
+        println!("{:?}", get_frame_header_as_bytes(3, true, false, 0, 32_u64))
+    }
+    #[test]
+    fn random(){
+        println!("{}", (true as u8));
+        println!("{}", (6 | 1))
+    }
+
 }
